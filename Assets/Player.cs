fa17e9d64f24f4a7ae0185a5fe2054cf13d435c6
unknown <aba26@mail.aub.edu>
2022-05-09 11:20:36 -0400

    "Loot - Health Bar - Zombie Asset"

---
+++ Assets/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int health = 100;
+    public int armor; 
+    public int score = 0;
+    public Health_Bar health_bar;
+    void Start()
+    {
+        armor = 25;
+        health_bar.SetMaxHealth(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void equipArmor(int arm)
+    {
+        armor += arm;
+    }
+
+    public void dealDamage(int damage)
+    {
+        int actual_damage = damage - armor;
+        health -= actual_damage;
+        armor--;
+        health_bar.SetHealth(50);
+    }
+
+    public int getHealth()
+    {
+        return health; 
+    }
+
+    public void healPlayer(int health_num)
+    {
+        health += health_num;
+    }
+
+    public void incrementScore()
+    {
+        score++;
+    }
+
+}
---
+++ Assets/Rotate_Float.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Float: MonoBehaviour
+{
+
+
+
+    //adjust this to change speed
+    float speed = 1f;
+    //adjust this to change how high it goes
+    float height = 0.5f;
+    // Start is called before the first frame update
+    Vector3 pos;
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 50 * Time.deltaTime, Space.Self);
+    }
+}
